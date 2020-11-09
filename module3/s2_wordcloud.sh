f3c17e5678673c06dfca68d54d2e8ef7c787bef1
Nicolas <nicolas.heroufosse@hotmail.com>
2020-11-09 19:43:19 +0100

    Update s2_wordcloud.sh

--- module3/s2_wordcloud.sh
+++ module3/s2_wordcloud.sh
@@ -2,7 +2,7 @@
 
 # Building a wordcloud based on one year of bulletins
 
-YEAR=$1
+YEAR=1956
 cat data/txt/*_${YEAR}_*.txt > data/${YEAR}.txt
 python module3/filtering.py 'data' $YEAR
 wordcloud_cli --text data/${YEAR}_keywords.txt --imagefile data/${YEAR}.png --width 2000 --height 1000
