2bd2bf78bcd7df899266ee445bf38cbb86d93c79
Talyonn <73362437+Talyonn@users.noreply.github.com>
2020-12-21 20:25:50 +0100

    Update s1_convert.sh

--- module2/s1_convert.sh
+++ module2/s1_convert.sh
@@ -3,8 +3,8 @@
 # Converting PDFs to text files and moving them to a new directory
 
 for file in data/pdf/*.pdf; do pdftotext -enc UTF-8 "$file" "${file%.*}.txt"; done
-mkdir data/txt
+mkdir data/hopitaux
 mv data/pdf/*.txt data/txt/
-ls data/txt | wc -l
-cat data/txt/*.txt > data/all.txt
-wc data/all.txt
+ls data/vote | wc -l
+cat data/hopitaux/all/*.txt > data/hopitaux/allvSH.txt
+wc data/hopitaux/allvSH.txt
